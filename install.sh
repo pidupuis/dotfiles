0e0632a84e8dd6c761e26d1635e595b91e4219f7
Pierre Dupuis <pidupuis@users.noreply.github.com>
2014-12-22 16:08:29 +0100

    Install fast and update global system

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 gsettings set org.gnome.desktop.session idle-delay 0
 
+## INSTALL APT-FAST
+sudo apt-add-repository ppa:saiarcot895/myppa -y && sudo apt-get update && sudo apt-get -y install apt-fast
+## UPDATE SYSTEM
+sudo apt-fast -y --force-yes update && sudo apt-fast -y --force-yes upgrade
+
 ## DOTFILES
 cd dotfiles && source bootstrap.sh --force
