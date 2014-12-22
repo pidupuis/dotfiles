32e4e7f9cc1a9ae649c52d52bb7615770a4f47b2
Pierre Dupuis <pidupuis@users.noreply.github.com>
2014-12-22 16:07:34 +0100

    Deactivate screen saver

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+## DEACTIVATE SCREEN SAVER
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
+gsettings set org.gnome.desktop.session idle-delay 0
+
 ## DOTFILES
 cd dotfiles && source bootstrap.sh --force
